4f158cd31d65f88ff35716692de97c7f214fe3de
Simon Smith <simon@smnth.dev>
2022-03-05 22:42:43 -0500

    Add clock

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,29 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let time = new Date();
+	$: hour = time.getHours();
+	$: minute = time.getMinutes();
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			time = new Date();
+		}, 1000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
+</script>
+
 <svelte:head>
 	<title>Home - Start Page</title>
 </svelte:head>
 
-<div class="">Test</div>
+<div class="grid grid-rows-4">
+	<div />
+	<div class="mx-auto">
+		<h2 class="text-8xl text-slate-50">{hour} : {minute}</h2>
+	</div>
+	<div />
+</div>
